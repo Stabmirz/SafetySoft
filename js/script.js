797e7f4fdaa449a880508d2f485944599b23eaab
Mirza <amirza.techlaunch.io>
2019-08-03 22:35:46 -0400

    updated

---
+++ js/script.js
@@ -0,0 +1,39 @@
+//faq toggle stuff 
+$('.togglefaq').click(function(e) {
+    e.preventDefault();
+    var notthis = $('.active').not(this);
+    notthis.find('.icon-up').addClass('icon-down').removeClass('icon-up');
+    notthis.toggleClass('active').next('.faqanswer').slideToggle(300);
+     $(this).toggleClass('active').next().slideToggle("fast");
+    $(this).children('i').toggleClass('icon-down icon-up');
+});
+
+var $_ = {
+    init: function () {
+        this.liftUp();
+    },
+
+    liftUp: function () {
+        $liftUp = $('.js-lift-up');
+
+        $(window).scroll(function(){
+            if ($(this).scrollTop() > 500) {
+                $liftUp.addClass('_active');
+            } else {
+                $liftUp.removeClass('_active');
+            }
+        });
+
+        $liftUp.click(function(){
+            $("html, body").animate({ scrollTop: 0 }, 600);
+            return false;
+        });
+    },
+
+};
+
+
+$(document).ready(function () {
+    $_.init();
+});
+
