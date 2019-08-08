170254c753df0f083861afd9d6feed5a641dc799
Mirza <amirza.techlaunch.io>
2019-08-08 17:18:33 -0400

    updated

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@
 $('.togglefaq').click(function(e) {
     e.preventDefault();
     var notthis = $('.active').not(this);
-    notthis.find('.icon-up').addClass('icon-down').removeClass('icon-up');
+    notthis.find('.icon-up').addClass('ques-icon').removeClass('icon-up');
     notthis.toggleClass('active').next('.faqanswer').slideToggle(300);
      $(this).toggleClass('active').next().slideToggle("fast");
-    $(this).children('i').toggleClass('icon-down icon-up');
+    $(this).children('i').toggleClass('ques-icon icon-up');
 });
 
 
